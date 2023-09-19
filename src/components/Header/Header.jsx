31af2ff4c8f091b73e3eb280c4316d116dce3c21
Mohammad Ashif <mohammadashif510@gmail.com>
2023-09-20 03:00:27 +0600

    dynamic navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { TbAlienFilled } from "react-icons/tb";
+import { AiOutlineBars, AiOutlineClose } from "react-icons/ai";
+import {BiSolidLogIn} from "react-icons/bi";
+ 
 const Header = () => {
+
+    const [open, setOpen] = useState(false);
+
+
+  
     return (
-        <nav>
-            <Link></Link>
-            <Link to='/'>First Page</Link>
-            <Link to='/foods'>Foods</Link>
-            {/* <a href="">Fist Page</a>
-            <a href="">Foods</a>
-            <a href="">Countries</a>
-            <a href="">Gym</a>
-            <a href="">Weather</a> */}
+        <nav className="flex gap-5 justify-center items-center justify-between">
+            <div className="flex-grow">
+                <Link to="/" className="text-4xl">
+                    <TbAlienFilled/>
+                </Link>
+            </div>
+            <div className={`md:flex lg:flex lg:flex-row lg:sticky lg:bg-white gap-5 flex-grow absolute duration-1000 bg-red-400 flex flex-col right-0 top-16 ${open? 'right-0 top-20': '-top-20'}`}>
+                <Link to="/">
+                    First Page
+                </Link>
+                <Link to="/foods">
+                    Foods
+                </Link>
+            </div>
+            <div className='text-3xl md:hidden ' onClick={() => setOpen(!open)}>
+                <Link>
+                    {
+                        open === true ? <AiOutlineClose/>: <AiOutlineBars/>
+                    }
+                </Link>
+            </div>
+            <div className='text-3xl md:block hidden'>
+                <Link>
+                    <BiSolidLogIn/>
+                </Link>
+            </div>
         </nav>
     );
 };
